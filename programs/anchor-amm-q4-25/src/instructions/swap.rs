f6916bced53cae0ca696839839d0670d40d193a2
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2026-02-03 22:52:38 +0530

    swap struct has been written

--- programs/anchor-amm-q4-25/src/instructions/swap.rs
+++ programs/anchor-amm-q4-25/src/instructions/swap.rs
@@ -7,20 +7,65 @@ use constant_product_curve::{ConstantProduct, LiquidityPair};
 
 use crate::{errors::AmmError, state::Config};
 
-// #[derive(Accounts)]
-// pub struct Swap<'info> {
-// TODO: Write the accounts struct
-// }
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Swap<'info> {
-//     pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
-// TODO
-//}
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
 
-//     pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
-// TODO
-//}
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+    #[account(
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config", config.seed.to_be_bytes().as_ref()],
+        bump = config.config_bump
+    )]
+    pub config: Account<'info, Config>,
 
-//     pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {}
-// TODO
-//}
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Swap<'info> {
+    pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
+TODO
+}
+
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+TODO
+}
+
+    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {}
+TODO
+}
