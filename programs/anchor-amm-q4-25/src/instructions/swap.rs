3765311c235e8a487bbac98f8bb82a8e1c2c9943
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2026-02-04 22:41:04 +0530

    test template is ready

--- programs/anchor-amm-q4-25/src/instructions/swap.rs
+++ programs/anchor-amm-q4-25/src/instructions/swap.rs
@@ -28,9 +28,10 @@ pub struct Swap<'info> {
     )]
     pub vault_y: Account<'info, TokenAccount>,
     #[account(
+        mut,
         has_one = mint_x,
         has_one = mint_y,
-        seeds = [b"config", config.seed.to_be_bytes().as_ref()],
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
         bump = config.config_bump
     )]
     pub config: Account<'info, Config>,
@@ -59,13 +60,86 @@ pub struct Swap<'info> {
 
 impl<'info> Swap<'info> {
     pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
-TODO
-}
+        require!(self.config.locked == false, AmmError::PoolLocked);
+        require!(amount > 0, AmmError::InvalidAmount);
+
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.vault_x.amount,
+            self.config.fee,
+            None,
+        )
+        .unwrap();
+
+        let pair = match is_x {
+            true => LiquidityPair::X,
+            false => LiquidityPair::Y,
+        };
+
+        let swap_result = curve.swap(pair, amount, min).unwrap();
+
+        require!(swap_result.deposit > 0, AmmError::InvalidAmount);
+        require!(swap_result.withdraw > 0, AmmError::InvalidAmount);
+
+        self.deposit_tokens(is_x, swap_result.deposit)?;
+        self.withdraw_tokens(is_x, swap_result.withdraw)?;
+
+        Ok(())
+    }
 
     pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
-TODO
-}
+        let (from, to) = match is_x {
+            true => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (
+                self.user_y.to_account_info(),
+                self.vault_y.to_account_info(),
+            ),
+        };
 
-    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {}
-TODO
+        let transfer_accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(self.token_program.to_account_info(), transfer_accounts);
+
+        transfer(cpi_ctx, amount)
+    }
+
+    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.vault_y.to_account_info(),
+                self.user_y.to_account_info(),
+            ),
+            false => (
+                self.vault_x.to_account_info(),
+                self.user_x.to_account_info(),
+            ),
+        };
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"config",
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump],
+        ]];
+
+        let tranfer_accounts = Transfer {
+            from,
+            to,
+            authority: self.config.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            tranfer_accounts,
+            signer_seeds,
+        );
+
+        transfer(cpi_ctx, amount)
+    }
 }
--- tests/anchor-amm-q4-25.ts
+++ tests/anchor-amm-q4-25.ts
@@ -1,16 +1,41 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorAmmQ425 } from "../target/types/anchor_amm_q4_25";
+import { getAssociatedTokenAddressSync, createAssociatedTokenAccountInstruction, createMint, mintTo, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 describe("anchor-amm-q4-25", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.anchorAmmQ425 as Program<AnchorAmmQ425>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  const user = provider.wallet.publicKey;
+  const admin = anchor.web3.Keypair.generate();
+
+  let mintX: anchor.web3.PublicKey;
+  let mintY: anchor.web3.PublicKey;
+  let userXAta: anchor.web3.PublicKey;
+  let userYAta: anchor.web3.PublicKey;
+  let vaultXAta: anchor.web3.PublicKey;
+  let vaultYAta: anchor.web3.PublicKey;
+  let userLpAta: anchor.web3.PublicKey;
+
+  // PDAs
+  let configPda: anchor.web3.PublicKey;
+  let configBump: number;
+  let mintLpPda: anchor.web3.PublicKey;
+  let mintLpBump: number;
+
+  const seeds = new anchor.BN(1234);
+
+
+  before(async () => {
+    await provider.connection.requestAirdrop(user, 10 * anchor.web3.LAMPORTS_PER_SOL);
+    await provider.connection.requestAirdrop(admin.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    mintX = await createMint(provider.connection, provider.wallet.payer, user, null, 0);
+    mintY = await createMint(provider.connection, provider.wallet.payer, user, null, 0)
+  })
 });
